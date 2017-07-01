1081a211c67dfe818ead5b5149a0a3958e972519
zakirmd <muhammad.zakir@axisrooms.com>
2017-07-01 23:58:58 +0530

    New UI v1

--- public/app/base/base.controllers.js
+++ public/app/base/base.controllers.js
@@ -3,6 +3,21 @@ angular.module("base.controllers", [
     ])
     .controller('BaseController', ['$scope', '$rootScope', function($scope, $rootScope) {
 
-        
-        
+       $scope.hotels = [{
+       	id:1,
+       	name: "Hotel Krishna"
+       },{
+       	id:2,
+       	name: "Ganga Residency"
+       },{
+       	id:3,
+       	name: "Paradise Hotel"
+       },{
+       	id:4,
+       	name: "Taj Vivanta"
+       }];
+       console.log($scope.hotels);
+
+       $scope.selectedHotel = $scope.hotels[0];
+
     }]);
