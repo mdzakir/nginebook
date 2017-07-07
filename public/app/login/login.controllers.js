ef43d5b14a428f1972f92a78c7614928fefeb0e4
zakirmd <muhammad.zakir@axisrooms.com>
2017-07-07 18:59:53 +0530

    Sidebar menu Categorized and Registration form

--- public/app/login/login.controllers.js
+++ public/app/login/login.controllers.js
@@ -6,17 +6,65 @@ angular.module("login.controllers", [
     $scope.title = "Login";
     $scope.$emit("pageTitleChanged", "Login");
 
+    $scope.loginForm = true;
+
+    $scope.loginOrRegistration = function(form){
+        if(form == "registration") {
+            $scope.loginForm = false;
+            $scope.registrationForm = true;
+        }
+        if(form == "login") {
+            $scope.registrationForm = false;
+            $scope.loginForm = true;
+        }
+    };
+
+    $scope.loginDetails = {
+        email_id : '',
+        password: '',
+        rememberMe: ''
+    };
+
+    $scope.registrationDetails = {
+        firstName : '',
+        lastName : '',
+        typeOfUser : '',
+        emailId : '',
+        mobile : '',
+        password: '',
+        confirmPassword: ''
+    }
     
 
     $scope.loginSubmit = function() {
-        User.login($scope.email_id, $scope.password).then(function() {
+        User.login($scope.loginDetails.email_id, $scope.loginDetails.password).then(function() {
             if (User.getToken('auth-token') == 'invalid') {
                 $scope.invalidLogin = true;
             } else {
                 $scope.invalidLogin = false;
             }
-
         });
+    };
+
+    $scope.registrationSubmit = function() {
+
+        var regParams = {
+            firstName : $scope.registrationDetails.firstName,
+            lastName : $scope.registrationDetails.lastName,
+            typeOfUser : $scope.registrationDetails.typeOfUser,
+            emailId : $scope.registrationDetails.emailId,
+            mobile : $scope.registrationDetails.mobile,
+            password: $scope.registrationDetails.password,
+            confirmPassword: $scope.registrationDetails.confirmPassword
+        };
+
+        // User.registration(params).then(function() {
+        //     if (User.getToken('auth-token') == 'invalid') {
+        //         $scope.invalidLogin = true;
+        //     } else {
+        //         $scope.invalidLogin = false;
+        //     }
+        // });
     };
 
 });
